d94538020f207a5a7e35db718ce638cf6c6149e0
Sam Malikin <workmalikin@gmail.com>
2025-11-05 21:29:25 +0200

    small update

--- src/__tests__/testData/index.ts
+++
@@ -1,5 +0,0 @@
-// src/__tests__/testData/index.ts
-
-export { adminTestData } from "./adminTestData.test.ts";
-export { userTestData } from "./userTestData.test.ts";
-export { employeeTestData } from "./employeeTestData.test.ts";
